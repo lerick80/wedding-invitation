b7a163fdec05a6cec92247a1cf122cd15b471db5
LOERICK <erick.lopez_gonzalez@daimlertruck.com>
2026-05-15 08:22:37 -0600

    navbar actualizada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,34 +15,63 @@ export default function Navbar() {
 
   return (
     <nav
-      className={`fixed w-full z-50 px-6 py-4 transition-all ${
+      className={`fixed w-full z-50 transition-all ${
         scrolled
           ? "bg-black/80 backdrop-blur text-white"
           : "bg-transparent text-white"
       }`}
     >
-      <div className="flex justify-center gap-8 text-sm tracking-widest">
+      <div className="flex justify-center items-center px-4 py-3 md:px-6 md:py-4">
 
-        {/* Scroll interno */}
-        <a href="#inicio" className="hover:opacity-70">
-          INICIO
-        </a>
+        {/* menu desk */}
+        <div className="hidden md:flex gap-8 text-sm tracking-widest">
 
-        <a href="#evento" className="hover:opacity-70">
-          EVENTO
-        </a>
+          <a href="#inicio" className="hover:opacity-70">
+            INICIO
+          </a>
 
-        <a href="#countdown" className="hover:opacity-70">
-          CUENTA REGRESIVA
-        </a>
+          <a href="#evento" className="hover:opacity-70">
+            EVENTO
+          </a>
 
-        <a href="#rsvp" className="hover:opacity-70">
-          RSVP
-        </a>
+          <a href="#countdown" className="hover:opacity-70">
+            CUENTA REGRESIVA
+          </a>
 
-        <a href="#contacto" className="hover:opacity-70">
-          CONTACTO
-        </a>
+          <a href="#rsvp" className="hover:opacity-70">
+            RSVP
+          </a>
+
+          <a href="#contacto" className="hover:opacity-70">
+            CONTACTO
+          </a>
+
+        </div>
+
+        {/* menu celular */}
+        <div className="flex md:hidden gap-4 text-[11px] tracking-wide">
+
+          <a href="#inicio" className="hover:opacity-70">
+            INICIO
+          </a>
+
+          <a href="#evento" className="hover:opacity-70">
+            EVENTO
+          </a>
+
+          <a href="#countdown" className="hover:opacity-70">
+            TIEMPO
+          </a>
+
+          <a href="#rsvp" className="hover:opacity-70">
+            RSVP
+          </a>
+
+          <a href="#contacto" className="hover:opacity-70">
+            CONTACTO
+          </a>
+
+        </div>
 
       </div>
     </nav>
